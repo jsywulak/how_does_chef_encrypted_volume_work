da4789868617656c8e9486e8c86aaf864445d3ae
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-05-20 09:47:42 -0500

    can't put the instance id in the custom json, so just put the stackname and we'll look it up from there.

--- missing_pieces/recipes/default.rb
+++ missing_pieces/recipes/default.rb
@@ -11,13 +11,15 @@ end
 # that's a sure sign you shouldn't use this code anywhere close to any real life application.
 # you're not even reading the comments, are you.
 
-# make the volume
+# find the volume
 require 'aws-sdk'
 volumes = AWS::EC2.new(region: 'us-west-2').volumes
 volume = volumes.select {|v| v.id == node[:volume]}.first
+stack = cfn.stacks.select{|c| c.name == node[stack_name]}
+ops_instance_id = stack.resources.select { |r| r.resource_type == "AWS::OpsWorks::Instance"}.stack_resource_detail.physical_resource_id
+ops.describe_instances(instance_ids: [stack.ops_instance_id])[:instances].first[:ec2_instance_id]
 
 # attach the volume
-opsworks_instance_id = node[:instance_id]
 ec2_instance_id = opsworks.describe_instances(instance_ids: [opsworks_instance_id])[:instances].first[:ec2_instance_id]
 instance = ec2.instances.select{ |i| i.id == ec2_instance_id }.first
 
@@ -25,3 +27,11 @@ volume.attach_to(instance, 'xvdf')
 
 
 
+
+
+  instance_id = nil
+  cfn.describe_stack_resources(stack_name: stack_name).stack_resources.each do |resource|
+    if resource.resource_type == "AWS::OpsWorks::Instance"
+      instance_id = cfn.describe_stack_resource(stack_name: stack_name, logical_resource_id: resource.logical_resource_id).stack_resource_detail.physical_resource_id
+    end
+  end
