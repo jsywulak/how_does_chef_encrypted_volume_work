b77d1780486ade6ec0772c0744a2a3ce24ea51b0
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-05-20 09:33:52 -0500

    okay, this might break in a different way now.

--- missing_pieces/recipes/default.rb
+++ missing_pieces/recipes/default.rb
@@ -14,10 +14,14 @@ end
 # make the volume
 require 'aws-sdk'
 volumes = AWS::EC2.new(region: 'us-west-2').volumes
-volume = volumes.create availability_zone: 'us-west-2a', size: 10
+volume = volumes.select {|v| v.id == node[:volume]}.first
 
 # attach the volume
-id = node[:instance_id]
-instance = ec2.instances.select{ |i| i.id == id }.first
+opsworks_instance_id = node[:instance_id]
+ec2_instance_id = opsworks.describe_instances(instance_ids: [opsworks_instance_id])[:instances].first[:ec2_instance_id]
+instance = ec2.instances.select{ |i| i.id == ec2_instance_id }.first
 
 volume.attach_to(instance, 'xvdf')
+
+
+
